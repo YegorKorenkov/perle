be6c2a6d53d2986477807266f0df6d8ad7967629
Yegorka <eg.korenkov@gmail.com>
2020-07-25 00:35:03 +0300

    +popup

---
+++ js/common.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$(".popup").magnificPopup();
+	$(".count_element").on("click", (function() {
+		ga("send", "event", "goal", "goal");
+		yaCounterXXXXXXXX.reachGoal("goal");
+		return true;
+	}));
+
+	$("#form").submit(function(e) {
+		e.preventDefault;
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Thanks for order");
+			setTimeout(function() {
+			}, 1000);
+		});
+	});
+});
